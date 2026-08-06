1092f996f464f1463c52ceb46756426c39e3dd30
tothand95 <tothand95@gmail.com>
2026-08-07 01:09:15 +0200

    lint and settings

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,28 @@
 // @ts-check
-const eslint = require('@eslint/js');
-const { defineConfig } = require('eslint/config');
-const tseslint = require('typescript-eslint');
-const angular = require('angular-eslint');
-const prettierRecommended = require('eslint-plugin-prettier/recommended');
+import eslint from '@eslint/js';
+import angular from 'angular-eslint';
+import prettierConfig from 'eslint-config-prettier';
+import prettierPlugin from 'eslint-plugin-prettier';
+import { defineConfig } from 'eslint/config';
+import typescriptEslint from 'typescript-eslint';
 
 module.exports = defineConfig([
   {
     files: ['**/*.ts'],
     extends: [
       eslint.configs.recommended,
-      tseslint.configs.recommended,
-      tseslint.configs.stylistic,
-      angular.configs.tsRecommended,
-      prettierRecommended,
+      ...typescriptEslint.configs.recommended,
+      ...typescriptEslint.configs.stylistic,
+      ...angular.configs.tsRecommended,
+      prettierConfig,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     processor: angular.processInlineTemplates,
     rules: {
+      'prettier/prettier': 'error',
+
       '@angular-eslint/directive-selector': [
         'error',
         {
@@ -33,13 +39,25 @@ module.exports = defineConfig([
           style: 'kebab-case',
         },
       ],
+      curly: 'warn',
+      'no-alert': 'error',
+      'no-lone-blocks': 'error',
+      'no-lonely-if': 'error',
+      'no-multi-assign': 'error',
+      'no-nested-ternary': 'error',
+      'no-implicit-coercion': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
     },
   },
   {
     files: ['**/*.html'],
     extends: [angular.configs.templateRecommended, angular.configs.templateAccessibility],
-    rules: {
-      '@angular-eslint/template/label-has-associated-control': 'off',
-    },
+    rules: {},
   },
 ]);
